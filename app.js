9cd8ba98db0212d1a954353827624694aa3d0186
Shivam <sg641818@gmail.com>
2025-06-29 00:24:33 +0530

    Enhance CORS configuration: specify allowed origin and enable credentials

--- app.js
+++ app.js
@@ -11,7 +11,13 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+
+app.use(cors({
+    origin: ['https://carrental-one.vercel.app/'],
+    credentials: true
+}));
+
+// app.use(cors())
 
 
 
