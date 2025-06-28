a165dd458408aca571741a5f3af75c533f9a80b9
Shivam <sg641818@gmail.com>
2025-06-29 00:34:14 +0530

    Fix CORS configuration: remove trailing slash from allowed origin

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['https://carrental-one.vercel.app/'],
+    origin: ['https://carrental-one.vercel.app'],
     credentials: true
 }));
 
