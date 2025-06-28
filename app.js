91ccec3b3abd0e6a622b1b39ee2296d25c876a3a
Shivam <sg641818@gmail.com>
2025-06-28 23:16:46 +0530

    Refactor server setup: move server listening logic to server.js

--- app.js
+++ app.js
@@ -29,10 +29,6 @@ app.get('/car', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
 
 
 export default app
